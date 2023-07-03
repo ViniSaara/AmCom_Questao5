11b029e6c5498777f902445abf2eed8ad847fe62
Vinícius Sabino <viniaraujobh2000@gmail.com>
2023-07-03 11:01:15 -0300

    Criando conta corrente controller.

---
+++ Application/Controllers/ContaCorrenteController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Questao5.Application.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ContaCorrenteController : ControllerBase
+    {
+        
+    }
+}
